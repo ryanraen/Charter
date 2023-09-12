1f6b64ff90f023eb8dc7ce2cce4143f382fbdf8c
Raenrein <mgthediamond@gmail.com>
2023-09-11 20:27:14 -0700

    Add backend template

---
+++ Backend/src/test/java/com/ryan/taskManager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.taskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
