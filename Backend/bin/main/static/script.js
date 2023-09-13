214b3265fcde2e6cbc32227a44fafe7132f0d065
ryan xie <mgthediamond@gmail.com>
2023-09-13 13:34:13 -0700

    testing backend to frontend request

---
+++ Backend/bin/main/static/script.js
@@ -0,0 +1,21 @@
+function submitForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+
+    // Send data to the backend
+    fetch('/process', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: name, email: email })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Handle the response from the backend
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
