032d48b4e7446896efa7ea48997e60a81945ed7e
ryan xie <mgthediamond@gmail.com>
2023-09-13 12:59:27 -0700

    user class

--- Backend/src/main/java/com/ryan/taskManager/User.java
+++ Backend/src/main/java/com/ryan/taskManager/User.java
@@ -11,7 +11,7 @@ public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
 
-    private String id;
+    private int id;
     private String username;
     private String email;
 
