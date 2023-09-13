ee93bf66ae457764829f3a3bf44a95be18f31acb
Raenrein <mgthediamond@gmail.com>
2023-09-12 22:59:21 -0700

    backend switch to maven and create User class, UserRepository class

--- Backend/src/main/java/com/ryan/taskManager/TaskManagerApplication.java
+++ Backend/src/main/java/com/ryan/taskManager/TaskManagerApplication.java
@@ -1,4 +1,4 @@
-package com.ryan.taskManager;
+package com.ryan.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,5 @@ public class TaskManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
-//hi
+
 }
