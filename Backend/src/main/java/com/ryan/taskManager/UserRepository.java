6683bf69ccfef9652383d315b24437813a19331d
ryan xie <mgthediamond@gmail.com>
2023-09-13 13:00:34 -0700

    a

--- Backend/src/main/java/com/ryan/taskManager/UserRepository.java
+++ Backend/src/main/java/com/ryan/taskManager/UserRepository.java
@@ -2,8 +2,6 @@ package com.ryan.taskmanager;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.ryan.taskmanager.User;
-
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     
