0213262995ee1b70b418da021fc89285d6d6e589
PenKK <spikeb3b@gmail.com>
2023-09-18 09:40:03 -0700

    Page swapping working

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -2,17 +2,17 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand navbar-dark bg-dark">
       <div className="container">
-        <a href="#" className="navbar-brand">
+        <a href="/" className="navbar-brand">
           Online ToDo Manager
         </a>
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a href="#" aria-current="page" className="nav-link">
+            <a href="/" aria-current="page" className="nav-link">
               Home
             </a>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link">
+            <a href="/login" className="nav-link">
               Log in
             </a>
           </li>
