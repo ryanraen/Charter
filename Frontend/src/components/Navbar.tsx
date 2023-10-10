e061bc71c1c152c54040b1c4bb29b6089e233c72
PenKK <spikeb3b@gmail.com>
2023-10-10 11:21:34 -0700

    dropdown more stuff

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import "./css/Navbar.css";
 import { useState } from "react";
 import gear from "../assets/gear.svg";
 import logout from "../assets/logout.svg";
+import downarrow from "../assets/dropdown.svg";
 
 const account = JSON.parse(localStorage.getItem("account") || "{}");
 const loggedIn = JSON.stringify(account) == "{}" ? false : true;
@@ -13,36 +14,17 @@ export default function Navbar() {
         <a href="/" className="navbar-brand">
           Charter
         </a>
-        <ul className="navbar-nav">
+        <ul className="navbar-nav" id="navbar-right">
           <NavItem href={loggedIn ? "/u" : "/"} item="Home" />
           {!loggedIn && <NavItem href="/login" item="Login" />}
-          <NavItem href="#" item={account.user_name}>
-            <DropdownMenu />
-          </NavItem>
+          {loggedIn && (
+            <NavItem href="#" item={account.user_name} styleid={"accountHeader"} rightIcon={downarrow}>
+              <DropdownMenu />
+            </NavItem>
+          )}
         </ul>
       </div>
     </nav>
-  );
-}
-
-function DropdownMenu() {
-  
-  function DropdownButton(props: any) {
-    return (
-      
-      <button className="btn dropdown-btn d-flex" onClick={props.func}>
-        <img src={props.leftIcon} alt="" className="dropdown-icon"/>
-        <div className="dropdown-label">{props.label}</div>
-        
-      </button>
-    );
-  }
-
-  return (
-    <div className="drop-down position-absolute d-flex flex-column p-3 rounded-3">
-      <DropdownButton label="Settings" leftIcon={gear} func={() => (location.href = "/settings")} />
-      <DropdownButton label="Log out" leftIcon={logout} func={() => {localStorage.removeItem("account"); location.href = "/";}} />
-    </div>
   );
 }
 
@@ -50,12 +32,38 @@ function NavItem(props: any) {
   const [open, setOpen] = useState(false);
 
   return (
-    <li className="nav-item">
-      <a href={props.href} className="nav-link" onClick={() => setOpen(!open)}>
+    <li className="nav-item navitem">
+      <a href={props.href} className="nav-link navlink" id={props.styleid} onClick={() => setOpen(!open)}>
         {props.item}
+        {props.rightIcon != undefined && <img src={props.rightIcon} alt="" width={15} height={15}/>}
       </a>
 
       {open && props.children}
     </li>
+  );
+}
+
+function DropdownMenu() {
+  function DropdownButton(props: any) {
+    return (
+      <button className="btn dropdown-btn d-flex" onClick={props.func}>
+        <img src={props.leftIcon} alt="" className="dropdown-icon" />
+        <div className="dropdown-label">{props.label}</div>
+      </button>
+    );
+  }
+
+  return (
+    <div className="drop-down position-absolute d-flex flex-column p-3 rounded-3">
+      <DropdownButton label="Settings" leftIcon={gear} func={() => (location.href = "/settings")} />
+      <DropdownButton
+        label="Log out"
+        leftIcon={logout}
+        func={() => {
+          localStorage.removeItem("account");
+          location.href = "/";
+        }}
+      />
+    </div>
   );
 }
