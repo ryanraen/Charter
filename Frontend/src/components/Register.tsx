1946d3efd3f8cc0a00a38fff376995c416f70cf8
PenK <spikeb3b@gmail.com>
2023-10-11 23:17:12 -0700

    Form validation started TODO: redo login/register

--- Frontend/src/components/Register.tsx
+++ Frontend/src/components/Register.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./css/LoginRegister.css";
 
 function submitNewUser(e: any) {
@@ -5,8 +6,10 @@ function submitNewUser(e: any) {
 
   const formData = new FormData(e.target);
   const formJson = Object.fromEntries(formData);
+  console.log(formJson);
+  
   localStorage.setItem("account", JSON.stringify(formJson));
-  window.location.href = "/u";
+  // window.location.href = "/u";
 
   // fetch("/addUser", {
   //   method: "POST",
@@ -26,21 +29,32 @@ function submitNewUser(e: any) {
 }
 
 export default function Register() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+  
   return (
     <div id="border-wrap">
       <div id="login-box" className="d-flex flex-column p-4 pt-5 pb-5">
         <form id="loginForm" className="text-center" onSubmit={submitNewUser}>
           <div className="form-floating">
-            <input id="name" name="user_name" type="username" className="form-control" placeholder="Username" required autoComplete="on" />
+            <input name="username" type="username" className="form-control" placeholder="Username" required />
             <label htmlFor="name">Username</label>
           </div>
           <div className="form-floating">
-            <input id="email" name="user_email" type="email" className="form-control" placeholder="Email" required autoComplete="on" />
+            <input name="email" type="email" className="form-control" placeholder="Email" required />
             <label htmlFor="email">Email</label>
           </div>
           <div className="form-floating">
-            <input id="password" name="user_password" type="password" className="form-control" placeholder="Password" required />
+            <input name="password" type="password" className="form-control" placeholder="Password" required />
             <label htmlFor="password">Password</label>
+          </div>
+          <div className="form-floating">
+            <input name="password" type="password" className="form-control" placeholder="ConfirmPassword" required />
+            <label htmlFor="password">Confirm Password</label>
           </div>
           <button type="submit" className="btn submit-btn">
             Register
