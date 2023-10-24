5f37c082fae61c00c35a6ec00e9859d4ca147129
PenK <spikeb3b@gmail.com>
2023-10-23 22:25:47 -0700

    aehoehgsjfgsujgshujnfhiujnsfghunijefue

--- Frontend/src/components/UserHome.jsx
+++ Frontend/src/components/UserHome.jsx
@@ -1,37 +1,36 @@
 import "./css/UserHome.css";
 import tableIcon from "../assets/table.svg";
+import plus from "../assets/plus.svg";
 
 export default function UserHome() {
   const tables = JSON.parse(localStorage.getItem("tables"));
 
-  function createTable(name) {
-    tables[name] = Math.random()
-    localStorage.setItem("tables", JSON.stringify(tables));
-  }
-
   return (
-    <div className="container border-black border d-flex">
+    <div className="container d-flex">
       <div id="side-bar" className="d-flex flex-column mt-5">
         <SideBarButton label="Tables" icon={tableIcon}></SideBarButton>
       </div>
-      <div className="d-flex border-black mt-5" id="tables">
-        {tables.toArray.map(table => (
-          <Table />
-        ))}
+      <div className="mt-5 w-100">
+        <div className="d-grid align-items-center h-100 w-100" id="tables">
+          <Table>
+            <h2>New table</h2>
+            <img src={plus} alt="" height={25} />
+          </Table>
+        </div>
       </div>
     </div>
   );
-}
 
-function SideBarButton(props) {
-  return (
-    <button className="btn side-bar-btn text-start d-flex align-items-center gap-3">
-      <img src={props.icon} height={20} alt="" className="side-button-icon" />
-      {props.label}
-    </button>
-  );
-}
+  function SideBarButton(props) {
+    return (
+      <button className="btn side-bar-btn text-start d-flex align-items-center gap-3" onClick={props.onClick}>
+        <img src={props.icon} height={20} alt="" className="side-button-icon" />
+        {props.label}
+      </button>
+    );
+  }
 
-function Table(props) {
-  return <div className="table-list-table"></div>;
+  function Table(props) {
+    return <div className="table-box p-2 align-items-center d-flex text-center flex-column">{props.children}</div>;
+  }
 }
