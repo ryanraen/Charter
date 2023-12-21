f0ca4f6c5cbae74ca7d6862967ea369157004ca4
PenKK <spikeb3b@gmail.com>
2023-12-21 14:49:56 -0800

    money

--- Frontend/src/util/CookieManager.jsx
+++ Frontend/src/util/CookieManager.jsx
@@ -3,23 +3,11 @@ export function setCookie(cookieName, cookieValue, expiryDate) {
   document.cookie = `${cookieName}=${cookieValue}; expires="${expiryDate}; path=/`;
 }
 
-export function getCookie(cookieName) {
-  let name = cookieName + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+export function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
 }
-
-
 
 export function deleteCookie(cookieName) {
   document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
