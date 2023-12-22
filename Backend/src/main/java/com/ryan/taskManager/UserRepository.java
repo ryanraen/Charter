1583fc9aa3844fbf404a6dbb69a5ac2a86bc7a4f
ryan xie <mgthediamond@gmail.com>
2023-12-22 11:18:42 -0800

    nullify access token

--- Backend/src/main/java/com/ryan/taskManager/UserRepository.java
+++ Backend/src/main/java/com/ryan/taskManager/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmailAndPassword(String email, String password);
+
+    List<Workspace> findWorkspaceByUserId(int userId);
 }
--- Backend/src/main/java/com/ryan/taskManager/WorkspaceRepository.java
+++ Backend/src/main/java/com/ryan/taskManager/WorkspaceRepository.java
@@ -2,8 +2,11 @@ package com.ryan.taskManager;
 
 import java.util.Optional;
 
+import org.hibernate.mapping.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WorkspaceRepository extends JpaRepository<Workspace, Integer> {
     
+    // List<workspace> findByUserID(Integer userID);
+
 }
