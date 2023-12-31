b411d1ed696b925896c89407bca9eb23be6b7db8
PenK <spikeb3b@gmail.com>
2023-12-30 23:53:58 -0800

    stuff

--- Frontend/src/components/Workspace.jsx
+++ Frontend/src/components/Workspace.jsx
@@ -1,12 +1,5 @@
-import { useParams } from "react-router-dom";
+
 
 export default function Workspace() {
-  const { id } = useParams();
-  return (
-    <div>
-      <div>
-        {id}
-      </div>
-    </div>
-  );
+  return <div>a</div>
 }
