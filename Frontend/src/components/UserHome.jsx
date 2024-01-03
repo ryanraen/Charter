a0bc87042eb01733106e73865557cfde100f040c
PenK <spikeb3b@gmail.com>
2024-01-02 17:50:04 -0800

    long names are accounted for

--- Frontend/src/components/UserHome.jsx
+++ Frontend/src/components/UserHome.jsx
@@ -2,7 +2,7 @@ import "./css/UserHome.css";
 import { getCookie } from "../util/CookieManager";
 import workspaceIcon from "../assets/workspace.svg";
 import plus from "../assets/plus.svg";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Modal from "./Modal";
 import { createWorkspace, getWorkspacesDisplay } from "../util/API";
 
@@ -11,10 +11,13 @@ export default function UserHome() {
     return;
   }
 
+  const defaultWorkspace = [{ name: "Loading...", id: "-2", isPublic: "Please wait" }];
+
   const [modalOpen, setModalOpen] = useState(false);
   const [disableModalSubmit, setDisableModalSubmit] = useState(false);
   const [modalErrorMessage, setModalErrorMessage] = useState("");
-  const [workspaces, setWorkspaces] = useState([{ name: "Loading...", id: "-2", isPublic: "Please wait" }]);
+
+  const [workspaces, setWorkspaces] = useState(defaultWorkspace);
 
   const keyPressEvent = e => {
     if (e.key === "Escape") {
@@ -32,11 +35,10 @@ export default function UserHome() {
       if (workspaceCreationJ.status == "success") {
         location.href = `/u/w/${workspaceCreationJ.id}`;
       }
-    } catch(error) {
+    } catch (error) {
       setModalErrorMessage("Server error, please try again later");
       setDisableModalSubmit(false);
     }
-
   }
 
   async function updateWorkspaces() {
@@ -45,7 +47,7 @@ export default function UserHome() {
       const workspacesJSON = workspacesString.map(string => JSON.parse(string));
       setWorkspaces(workspacesJSON);
     } catch (error) {
-      setWorkspaces([{ name: "Server error", id: "-2", isPublic: "Please try again later" }])
+      setWorkspaces([{ name: "Server error", id: "-2", isPublic: "Please try again later" }]);
       console.log(error);
     }
   }
@@ -55,11 +57,13 @@ export default function UserHome() {
       document.addEventListener("keydown", keyPressEvent);
     }
 
-    updateWorkspaces();
-
     return () => {
       document.removeEventListener("keydown", keyPressEvent);
     };
+  }, []);
+
+  useEffect(() => {
+    updateWorkspaces();
   }, []);
 
   const workspaceTextInputs = [
@@ -69,7 +73,7 @@ export default function UserHome() {
       type: "text",
       label: "Workspace name",
       placeholder: "Workspace name",
-      pattern: `^[\\S\\s]+[\\S]+$`,
+      pattern: `^[\\S\\s]+$`,
       errorMessage: "Invalid name",
       required: true,
     },
@@ -99,15 +103,11 @@ export default function UserHome() {
         </div>
         <div className="mt-5 w-100 container">
           <div className="d-grid align-items-center h-100 w-100" id="workspace">
-            <Workspace id={-1} onClick={() => setModalOpen(true)}>
-              <h3>New workspace</h3>
+            <Workspace title={"New Workspace"} id={-1} onClick={() => setModalOpen(true)}>
               <img src={plus} alt="" height={25} />
             </Workspace>
-            {workspaces.map(workspace => (
-              <Workspace key={workspace.id} onClick={workspace.id >= 0 ? () => (location.href = `/u/w/${workspace.id}`) : null}>
-                <h3>{workspace.name}</h3>
-                <h6>{workspace.isPublic === "true" ? "Public" : workspace.isPublic === "false" ? "Private" : workspace.isPublic}</h6>
-              </Workspace>
+            {workspaces.toReversed().map(workspace => (
+              <Workspace isPublic={workspace.isPublic} title={workspace.name} key={workspace.id} onClick={workspace.id >= 0 ? () => (location.href = `/u/w/${workspace.id}`) : null}></Workspace>
             ))}
           </div>
         </div>
@@ -136,8 +136,28 @@ export default function UserHome() {
   }
 
   function Workspace(props) {
+
+    function isOverflown(element) {
+      return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
+    }
+
+    const titleRefDiv = useRef(null);
+    const titleRef = useRef(null);
+
+    useEffect(() => {
+      if (isOverflown(titleRefDiv.current)) {
+        titleRef.current.innerText = titleRef.current.innerText.substring(0, 25).trim() + "...";
+      }
+    }, [titleRef.current])
+    
     return (
       <div role="button" onClick={props.onClick} className="workspace-box p-2 align-items-center d-flex text-center flex-column justify-content-center user-select-none">
+        <div ref={titleRefDiv} className="workspace-title d-flex align-items-center justify-content-center">
+          <h3 ref={titleRef}>{props.title}</h3>
+        </div>
+        <div className="workspace-privacy">
+          <h6>{props.isPublic === "true" ? "Public" : props.isPublic === "false" ? "Private" : props.isPublic}</h6>
+        </div>
         {props.children}
       </div>
     );
